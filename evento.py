29add62a2e0a3273edb68c402052b5aeca615709
iurroz <92694254+iurroz@users.noreply.github.com>
2021-10-17 18:53:26 -0300

    Add files via upload

---
+++ evento.py
@@ -0,0 +1,17 @@
+
+from sensor import EventoReportado
+
+
+
+class calor (EventoReportado):
+    def topTres(self):
+        pass
+
+    def rankin(self):
+        pass
+
+    def zona(self):
+        pass
+
+    def Coordenadas(self):
+        pass
---
+++ sensor.py
@@ -0,0 +1,5 @@
+from Publico import ReportarEvento
+
+class EventoReportado(ReportarEvento):
+    def unicoEvento ():
+        pass
